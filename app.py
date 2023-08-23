2f16cfe8e6b528b01fc1fd9bd38d98a3d55fa2df
Olalekan2040-slack <olalekanquadri58@gmail.com>
2023-08-23 09:24:45 -0800

    change index path

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('C:\Users\User\Documents\ImageToText\templates\index.html')
 
 
 
