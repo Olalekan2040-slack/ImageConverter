6cb3c953287b3e56a08f40bc14de38bfabfc02d6
Olalekan2040-slack <olalekanquadri58@gmail.com>
2023-08-24 11:56:40 -0800

    added downloadable converted  pdf and word document formats

--- ImageToText/app.py
+++ ImageToText/app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 import pytesseract
 from PIL import Image, UnidentifiedImageError
 import io
+from fpdf import FPDF
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("home.html")
-
-
-
 
 @app.route('/convert', methods=['POST'])
 def convert_image():
@@ -31,7 +29,28 @@ def convert_image():
     else:
         return render_template('home.html', no_text=True)
 
+@app.route('/convert_to_pdf', methods=['GET'])
+def convert_to_pdf():
+    extracted_text = request.args.get('text')
+    
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.multi_cell(0, 10, extracted_text)
+    
+    pdf_output = io.BytesIO()
+    pdf_output.seek(0)
+    pdf_output.write(pdf.output(dest='S').encode('latin1'))
+    pdf_output.seek(0)
+    
+    return Response(pdf_output, mimetype='application/pdf', headers={'Content-Disposition': 'attachment; filename=converted_text.pdf'})
 
+@app.route('/convert_to_word', methods=['GET'])
+def convert_to_word():
+    extracted_text = request.args.get('text')
+    
+    response = Response(extracted_text, mimetype='text/plain', headers={'Content-Disposition': 'attachment; filename=converted_text.txt'})
+    return response
 
 if __name__ == '__main__':
     app.run(debug=False)
